46c50621243f6c4824ad6053fcf0f72515ad2c33
Guilherme Pimenta <guilhermevpimenta122@gmail.com>
2026-03-06 18:45:49 -0300

    add snacks dashboard plugin

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,7 +5,6 @@ vim.pack.add({
 require("snacks").setup({
   picker = { enabled = true },
   bigfile = { enabled = true },
-  dashboard = { enabled = true },
   explorer = { enabled = true },
   quickfile = { enabled = true },
   animate = { enabled = true },
@@ -15,4 +14,39 @@ require("snacks").setup({
   statuscolumn = { enabled = true },
   words = { enabled = true },
   indent = { enabled = true },
+  dashboard = {
+    enabled = true,
+    sections = {
+    { section = "header" },
+    { section = "keys", gap = 1},
+  },
+    preset = {
+      header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+      ]],
+      keys = {
+        { icon = " ", key = "f", desc = "Find File", action = function() Snacks.dashboard.pick("files") end },
+
+        { icon = " ", key = "p", desc = "Projects", action = function() Snacks.picker.projects() end },
+
+        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+
+        { icon = "󰒲 ", key = "l", desc = "Leetcode", action = ":Leet" },
+
+        { icon = " ", key = "c", desc = "Config", action = function()
+          Snacks.dashboard.pick("files", { cwd = vim.fn.stdpath("config") })
+        end },
+
+        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          
+        { section = "recent_files", gap = 2 },
+      },
+    },
+  },
+
 })
