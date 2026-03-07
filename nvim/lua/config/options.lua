2cc45046f8b297dacaebb6ffe06250f6e54bc720
Guilherme Pimenta <guilhermevpimenta122@gmail.com>
2026-03-07 10:47:16 -0300

    chore: format options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -19,36 +19,36 @@ opt.undofile = true
 opt.undodir = vim.fn.stdpath("state") .. "/undo"
 
 vim.diagnostic.config({
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = "🛑",
-      [vim.diagnostic.severity.WARN]  = "⚠️",
-      [vim.diagnostic.severity.INFO]  = "ℹ️",
-      [vim.diagnostic.severity.HINT]  = "💡",
-    },
-    numhl = {
-      [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-      [vim.diagnostic.severity.WARN]  = "DiagnosticSignWarn",
-      [vim.diagnostic.severity.INFO]  = "DiagnosticSignInfo",
-      [vim.diagnostic.severity.HINT]  = "DiagnosticSignHint",
-    },
-  },
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "🛑",
+			[vim.diagnostic.severity.WARN] = "⚠️",
+			[vim.diagnostic.severity.INFO] = "ℹ️",
+			[vim.diagnostic.severity.HINT] = "💡",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+		},
+	},
 
-  virtual_text = {
-    spacing = 2,
-    source = "if_many",
-    prefix = function(diagnostic)
-      local icons = {
-        [vim.diagnostic.severity.ERROR] = "🛑",
-        [vim.diagnostic.severity.WARN]  = "⚠️",
-        [vim.diagnostic.severity.INFO]  = "ℹ️",
-        [vim.diagnostic.severity.HINT]  = "💡",
-      }
-      return icons[diagnostic.severity] .. " "
-    end,
-  },
+	virtual_text = {
+		spacing = 2,
+		source = "if_many",
+		prefix = function(diagnostic)
+			local icons = {
+				[vim.diagnostic.severity.ERROR] = "🛑",
+				[vim.diagnostic.severity.WARN] = "⚠️",
+				[vim.diagnostic.severity.INFO] = "ℹ️",
+				[vim.diagnostic.severity.HINT] = "💡",
+			}
+			return icons[diagnostic.severity] .. " "
+		end,
+	},
 
-  underline = true,
-  severity_sort = true,
-  update_in_insert = false,
+	underline = true,
+	severity_sort = true,
+	update_in_insert = false,
 })
